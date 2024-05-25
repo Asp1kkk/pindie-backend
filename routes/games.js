0d53e41b13a5b9d4ecd1c814ae3aa2e8915a43c9
Asp1kkk <a5p1k@yandex.ru>
2024-05-25 14:00:07 +0300

    add: PUT for /games/:id

--- routes/games.js
+++ routes/games.js
@@ -1,10 +1,10 @@
 const gamesRouter = require("express").Router();
 
-const { findAllGames, createGame, findGameById } = require("../middlewares/games");
-const { sendAllGames, sendCreatedGame, sendGameById } = require("../controllers/games");
+const { findAllGames, createGame, findGameById, updateGame } = require("../middlewares/games");
+const { sendAllGames, sendCreatedGame, sendGameById, sendUpdatedGame } = require("../controllers/games");
 
 gamesRouter.get("/games/:id", findGameById, sendGameById);
-
 gamesRouter.post("/games", findAllGames, createGame, sendCreatedGame);
+gamesRouter.put("/games/:id", findGameById, updateGame, sendUpdatedGame);
 
 module.exports = gamesRouter;
